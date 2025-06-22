a39da97b15b89e2fc1f048af6385a013b984ab4f
Shidhar25 <shridharhande13@gmail.com>
2025-06-22 23:21:17 +0530

    start

---
+++ src/test/java/org/springboot_jdbc/baithak/BaithakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot_jdbc.baithak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaithakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
