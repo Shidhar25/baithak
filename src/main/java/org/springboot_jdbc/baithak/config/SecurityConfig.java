23b9f08a5768b1dec476ad2248fcd235bdebca81
Shidhar25 <shridharhande13@gmail.com>
2025-07-01 18:37:58 +0530

    ..

--- src/main/java/org/springboot_jdbc/baithak/config/SecurityConfig.java
+++ src/main/java/org/springboot_jdbc/baithak/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React dev server
+                        .allowedOrigins("https://baithak-sigma.vercel.app")// React dev server
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
